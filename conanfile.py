688dfb420cf6dfa495ba93ddcee3a44ee2b1dd8c
Didier Weckmann <didier.weckmann@ircad.fr>
2019-04-02 11:43:14 +0200

    feat(conanfile): add package_revision support

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,16 @@
 import os
 import shutil
 
-from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
-from conans.util import files
+from conans import ConanFile, CMake, tools
+from fnmatch import fnmatch
 
 
 class LibCeresConan(ConanFile):
     name = "ceres"
-    short_version = "1.14.0"
-    version = "{0}-r1".format(short_version)
+    upstream_version = "1.14.0"
+    package_revision = "-r1"
+    version = "{0}{1}".format(upstream_version, package_revision)
+
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
@@ -29,12 +31,12 @@ class LibCeresConan(ConanFile):
 
     def requirements(self):
         self.requires("eigen/3.3.7@sight/testing")
-        self.requires("glog/0.3.5-rev-8d7a107@sight/stable")
-        self.requires("cxsparse/3.1.1@sight/stable")
+        self.requires("glog/0.4.0@sight/testing")
+        self.requires("cxsparse/3.1.1-r1@sight/testing")
 
     def source(self):
-        tools.get("http://ceres-solver.org/ceres-solver-{0}.tar.gz".format(self.short_version))
-        os.rename("ceres-solver-" + self.short_version, self.source_subfolder)
+        tools.get("http://ceres-solver.org/ceres-solver-{0}.tar.gz".format(self.upstream_version))
+        os.rename("ceres-solver-" + self.upstream_version, self.source_subfolder)
 
     def build(self):
         cxsparse_source_dir = os.path.join(self.source_folder, self.source_subfolder)
@@ -63,13 +65,29 @@ class LibCeresConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
 
     def cmake_fix_path(self, file_path, package_name):
-        tools.replace_in_file(
-            file_path,
-            self.deps_cpp_info[package_name].rootpath.replace('\\', '/'),
-            "${CONAN_" + package_name.upper() + "_ROOT}"
-        )
+        try:
+            tools.replace_in_file(
+                file_path,
+                self.deps_cpp_info[package_name].rootpath.replace('\\', '/'),
+                "${CONAN_" + package_name.upper() + "_ROOT}",
+                strict=False
+            )
+        except:
+            self.output.info("Ignoring {0}...".format(package_name))
 
     def package(self):
-        if not tools.os_info.is_windows:
-            ceres_config_file = os.path.join(self.package_folder, "lib", "cmake", "Ceres", "CeresConfig.cmake")
-            self.cmake_fix_path(ceres_config_file, "glog")
+        for path, subdirs, names in os.walk(self.package_folder):
+            for name in names:
+                if fnmatch(name, '*.cmake'):
+                    cmake_file = os.path.join(path, name)
+                    
+                    tools.replace_in_file(
+                        cmake_file, 
+                        self.package_folder.replace('\\', '/'), 
+                        '${CONAN_CERES_ROOT}', 
+                        strict=False
+                    )
+                    
+                    self.cmake_fix_path(cmake_file, "glog")
+                    self.cmake_fix_path(cmake_file, "cxsparse")
+                    self.cmake_fix_path(cmake_file, "eigen")
